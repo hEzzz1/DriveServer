c78755ef22007063e4717a02f19acac083874ae2
zhangyufeng <742724799@qq.com>
2026-04-30 13:43:50 +0800

    fix：设备绑定

--- src/main/resources/db/migration/V21__edge_device_remove_activation_code_dependency.sql
+++ src/main/resources/db/migration/V21__edge_device_remove_activation_code_dependency.sql
@@ -1,6 +1,13 @@
+ALTER TABLE edge_device
+  DROP CHECK chk_device_status;
+
 UPDATE edge_device
 SET status = CASE
-  WHEN status = 'DISABLED' THEN 'DISABLED'
+  WHEN status IN ('DISABLED', '0') THEN 'DISABLED'
   WHEN enterprise_id IS NULL THEN 'NEW'
   ELSE 'BOUND'
 END;
+
+ALTER TABLE edge_device
+  ADD CONSTRAINT chk_device_status
+    CHECK (status IN ('NEW', 'BOUND', 'DISABLED'));
